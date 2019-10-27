5d41294e48fc64c2c0dd7dc54c1d7d15d29c8a5c
Ben Cawrse <bcawse@gmail.com>
2019-10-27 15:57:16 -0400

    Adding device controller

---
+++ DeviceSensorWeb/Controllers/DeviceController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DeviceSensorWeb.CustomAttributes;
+
+namespace DeviceSensorWeb.Controllers
+{
+    public class DeviceController : Controller
+    {
+        // GET: Device
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Device/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Device/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Device/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Device/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Device/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Device/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Device/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
